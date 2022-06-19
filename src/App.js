533c13d79e1d8d0e575db4173999f41f14ac44ad
Lee <dltmdgns0508@gmail.com>
2022-06-19 22:55:47 +0900

    기본 구조 추가 및 라우팅 기본 설정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />}/>
+      </Routes>
     </div>
   );
 }
