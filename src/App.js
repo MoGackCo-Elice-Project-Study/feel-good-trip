aa0b1ad41ff7e0e200c851e709db1ff86e4da8db
Lee <dltmdgns0508@gmail.com>
2022-06-26 23:44:41 +0900

    추가 경로 및 파일 생성 + 메인화면 구성

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
+import Search from './pages/Search';
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />}/>
+        <Route path="/search" element={<Search />} />
       </Routes>
     </div>
   );
